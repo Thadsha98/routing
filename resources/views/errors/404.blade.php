df355235aa7de9fe2fbe4fada52ad85eeb05fe1a
Thadsha98 <shajiniraj1998@gmail.com>
2024-10-10 16:27:45 +0530

    commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page not found</title>
+</head>
+<body>
+<div class="error-container">
+    <h1>404</h1>
+    <p>Oops! The page you're looking for cannot be found.</p>
+    <p><a href="{{ url('/') }}">Go back to Homepage</a></p>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,50 @@
 <?php
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome to my laravel app";
+});
+
+
+Route::get('/user/{id}', function ($id) {
+    return 'User ID:'.$id;
+})->where ('id', '[0-9]+');
+
+
+Route::get('/profile/{name?}', function ($name ='Guest') {
+    return 'welcome:'.$name;
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return "Admin Users List";
+    });
+
+    Route::get('/settings', function () {
+        return "Admin Settings";
+    });
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/products/{product}', function (Product $product) {
+    return $product->name;
+});
+
+
+Route::redirect('/old-url', '/new-url' );
+
+Route::fallback(function () {
+    return "Oops, page not found!";
 });
